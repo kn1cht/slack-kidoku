e3951f5b1898920196ce68d8b85356df8794cebb
kn1cht <ito.turukasiwa@gmail.com>
2017-07-07 23:55:02 +0900

    Kidoku button interactive handler

--- app.js
+++ app.js
@@ -21,81 +21,88 @@ controller.setupWebserver(3000, () => {
   });
 });
 
-function kidokuButton(text, author) {
-  this.fallback        = 'Read confirmation button.';
-  this.callback_id     = 'slack-kidoku';
-  this.color           = 'good';
-  this.attachment_type = 'default';
-  this.text            = text;
-  this.actions         = [
-    {
-      name  : 'kidoku',
-      text  : '既読',
-      type  : 'button',
-      style : 'primary'
-    }
-  ];
-  this.footer          = author;
-}
-
-function kidokuConfirm(text, author) {
-  this.fallback        = 'Confirmation of read confirmation button.';
-  this.callback_id     = 'slack-kidoku-confirm';
-  this.attachment_type = 'default';
-  this.title           = '既読ボタンを作成します';
-  this.actions         = [
-    {
-      name  : 'ok',
-      text  : 'OK',
-      type  : 'button',
-      value : JSON.stringify({
-        author : author,
-        text   : text
-      }),
-      style : 'primary'
-    },
-    {
-      name  : 'cancel',
-      text  : 'Cancel',
-      type  : 'button',
-      style : 'danger'
-    }
-  ];
-}
-
 controller.on('slash_command', (bot, src) => {
   console.log(src);
   if(src.command === '/kidoku') {
-    const reply = {
-      text        : 'Preview:',
-      attachments : [
-        new kidokuButton(src.text, src.user_name),
-        new kidokuConfirm(src.text, src.user_name)
-      ],
-    };
-    bot.replyPrivate(src, reply);
+    usersInfo(src.user_id).then((res) => {
+      const attachments = [
+        new KidokuButton(src.text, res.user.name, res.user.profile.image_24),
+        new KidokuConfirm(src.text)
+      ];
+      bot.replyPrivate(src, { text : 'Preview:', attachments : attachments });
+    }).catch((err) => {
+      console.error(err);
+    });
   }
 });
 
 controller.on('interactive_message_callback', (bot, src) => {
   console.log(src);
   if (src.callback_id === 'slack-kidoku-confirm') {
-    const name = src.actions[0].name;
-    if (name === 'ok') {
-      const value = JSON.parse(src.actions[0].value);
+    if (src.actions[0].name === 'ok') {
       bot.replyInteractive(src, { text : 'Success!' });
-      controller.spawn({
-        token : config.botToken || ''
-      }).startRTM((err, bot) => {
-        if (err) { throw new Error('Could not connect to Slack'); }
-        bot.say({
-          attachments : [new kidokuButton(value.text, value.author)],
-          channel     : src.channel
-        });
-      });
+      postKidokuButtonToChannel(src.actions[0].value, src.user, src.channel);
     }
-    else if (name === 'cancel') {
+    else if (src.actions[0].name === 'cancel') {
       bot.replyInteractive(src, { text : 'Canceled :wink:' });
     }
   }
+  else if (src.callback_id === 'slack-kidoku') {
+    const attachments = src.original_message.attachments;
+    attachments[0].fields = [{
+      "title": "既読",
+      "value": src.user
+    }];
+    console.log(attachments);
+    bot.replyInteractive(src, { attachments : attachments });
+  }
 });
+
+const KidokuButton = function(text, name, icon) {
+  this.fallback        = 'Read confirmation button.';
+  this.callback_id     = 'slack-kidoku';
+  this.color           = 'good';
+  this.attachment_type = 'default';
+  this.author_name     = name;
+  this.author_icon     = icon;
+  this.text            = text;
+  this.actions         = [
+    { name : 'kidoku', text : '既読', type : 'button', style : 'primary' }
+  ];
+};
+
+const KidokuConfirm = function(text) {
+  this.fallback        = 'Confirmation of read confirmation button.';
+  this.callback_id     = 'slack-kidoku-confirm';
+  this.attachment_type = 'default';
+  this.title           = '既読ボタンを作成します';
+  this.actions         = [
+    { name : 'ok', text : 'OK', type : 'button', value : text, style : 'primary' },
+    { name : 'cancel', text : 'Cancel', type : 'button', style : 'danger' }
+  ];
+};
+
+function usersInfo(userId) {
+  return new Promise((resolve, reject) => {
+    controller.spawn({ token : config.botToken || '' })
+    .api.users.info({ user : userId }, (err, response) => {
+      if(err) { reject(err); }
+      else { resolve(response);  }
+    });
+  });
+}
+
+function postKidokuButtonToChannel(text, userId, channel) {
+  usersInfo(userId).then((res) => {
+    const attachments = [
+      new KidokuButton(text, res.user.name, res.user.profile.image_24),
+    ];
+    controller.spawn({ token : config.botToken || '' })
+    .startRTM((err, bot) => {
+      if (err) { throw new Error('Could not connect to Slack'); }
+      bot.say({ attachments : attachments, channel : channel });
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
+}
