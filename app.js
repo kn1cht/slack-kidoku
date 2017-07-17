d7795589912b3d46ea22a55871cb6ea4ecbad2e3
kn1cht <ito.turukasiwa@gmail.com>
2017-07-17 23:58:01 +0900

    show unread users

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ controller.on('slash_command', (bot, msg) => {
 });
 
 controller.on('interactive_message_callback', (bot, msg) => {
-  //console.log(msg);
+  console.log(msg);
   if(msg.callback_id === 'slack-kidoku-confirm') {
     if(msg.actions[0].name === 'cancel') {
       bot.replyInteractive(msg, { text : 'Canceled :wink:' });
@@ -61,17 +61,12 @@ controller.on('interactive_message_callback', (bot, msg) => {
         const result = await util.promisify(botUser.api.chat.postMessage) ({ channel : msg.channel, attachments : attachments, link_names : true });
         bot.replyInteractive(msg, { text : 'Success!' });
 
-        const text = result.message.attachments[0].text);
+        const text = result.message.attachments[0].text;
         const channelMention = text.match(/<!(.*?)>/g);
         const userMention = text.match(/<@(.*?)>/g);
         if(!channelMention && userMention) {
           const data = await util.promisify(controller.storage.channels.get) (msg.channel);
-          const item = data[key];
-          let allUser = [];
-          for(const user in userMention) {
-            allUser.push(text.substr(2,8));
-          }
-          data[key].all_user = allUser;
+          data[key].all_user = userMention.reduce((res, user) => [...res, user.substr(2, user.length - 3)], []); // <@U*********> -> U*********
           controller.storage.channels.save(data);
         }
       })().catch((err) => {
@@ -87,20 +82,28 @@ controller.on('interactive_message_callback', (bot, msg) => {
   }
   else if(msg.callback_id === 'slack-kidoku') {
     (async() => {
-      const attachments = [ msg.original_message.attachments[0] ]; // original text and button
       const data = await util.promisify(controller.storage.channels.get) (msg.channel);
       const item = data[msg.text];
-      if(item.read_user.indexOf(msg.user) >= 0) { // if the user already exists in read_user, delete them
-        item.read_user = item.read_user.filter((val) => (val !== msg.user));
+      if(msg.actions[0].name === 'kidoku') {
+        if(item.read_user.indexOf(msg.user) >= 0) { // if the user already exists in read_user, delete them
+          item.read_user = item.read_user.filter((val) => (val !== msg.user));
+        }
+        else { item.read_user.push(msg.user); } // if not, add them
+        data[msg.text] = item;
+        controller.storage.channels.save(data);
+
+        const attachments = [ msg.original_message.attachments[0] ]; // original text and button
+        attachments.push({
+          title : `既読(${item.read_user.length})`,
+          text  : item.read_user.reduce((pre, user) => `${pre}, <@${user}>`, '').slice(1, ) // concatenate user mentions
+        });
+        bot.replyInteractive(msg, { attachments : attachments });
       }
-      else { item.read_user.push(msg.user); } // if not, add them
-      data[msg.text] = item;
-      controller.storage.channels.save(data);
-      attachments.push({
-        title : `既読(${item.read_user.length}/${item.all_user.length})`,
-        text  : item.read_user.reduce((pre, user) => `${pre}, <@${user}>`, '').slice(1, ) // concatenate user mentions
-      });
-      bot.replyInteractive(msg, { attachments : attachments });
+      else if(msg.actions[0].name === 'show-unread') {
+        const unread_user = item.all_user.filter((user) => !item.read_user.includes(user));
+        const text = unread_user.reduce((pre, user) => `${pre}, <@${user}>`, '').slice(1, ); // concatenate user mentions
+        bot.replyInteractive(msg, { text : text , response_type : 'ephemeral' , replace_original : false });
+      }
     })().catch((err) => { console.error(err); });
   }
 });
@@ -117,7 +120,8 @@ const kidokuButton = (text, userId, option = {}) => {
       author_name     : info.user.name,
       author_icon     : info.user.profile.image_24,
       actions         : [
-        { name : 'kidoku', text : '既読', type : 'button', style : 'primary', value : option.value || '' }
+        { name : 'kidoku', text : '既読', type : 'button', style : 'primary', value : option.value || '' },
+        { name : 'show-unread', text : '未読者を表示', type : 'button', value : option.value || '' }
       ]
     };
   })().catch((err) => { console.error(err); });
